aec43a8b083404202dc0e59d810a9db9a9e52341
Mikedaniel <mikedaniel.ocasio@me.com>
2019-03-28 23:17:40 -0400

    fixed form

--- 2019_Portfilio/index.php
+++ 2019_Portfilio/index.php
@@ -204,12 +204,11 @@ if($_POST["message"]) {
     <input type="email" class="form-control" id="email">
 </div>
 <div class ="requests inline2">
-   <form method ="post" action="contact.php">
+   <form method ="post" action="index.php">
 <textarea name="message" class="form-control" rows="4" id="comment"></textarea>
     <input type="submit">
 </form>
 </div>
-
 
 </div>
 
